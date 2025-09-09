33bd09b4b3e59acaf4cbc4884754adc1eaf737c6
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2025-09-09 22:23:15 +0200

    fixture for innit data

--- django_backend/scripts/entrypoint.sh
+++ django_backend/scripts/entrypoint.sh
@@ -5,6 +5,9 @@ python3 /django/manage.py makemigrations tasks
 python3 /django/manage.py makemigrations
 python3 /django/manage.py migrate
 
+# Populating database
+python manage.py loaddata /django/apps/fixtures/seeding.json
+
 # celery
 celery -A config worker -l info &
 celery -A config beat &
